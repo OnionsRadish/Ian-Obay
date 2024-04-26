2505867de230706150cf665f3888663d855e2bd5
Ian Obay <168149002+OnionsRadish@users.noreply.github.com>
2024-04-26 15:13:05 +0800

    Add files via upload

---
+++ number-challenge.js
@@ -0,0 +1,23 @@
+/*
+    1 Create a function that will return the sum 3 numbers
+    2 Create a function that will return the difference of the 2 numbers
+    3 Create a function that will return the product of the 2 numbers
+    4 Create a function that will return the average of the 2 numbers
+*/
+
+function funcadd(a, b) {
+    return a + b;
+  }
+function funcsub(a, b) {
+    return a - b;
+  }
+function funcmulti(a, b) {
+    return a * b;
+  }
+function funcdiv(a, b) {
+    return a / b;
+  }
+console.log(funcadd(5, 5));
+console.log(funcsub(5, 5));
+console.log(funcmulti(5, 5));
+console.log(funcdiv(5, 5));
